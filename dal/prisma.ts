819e397e5a9613ea1301caa430338d7a05a0ff96
Ben Beacham <deadpangameryt@gmail.com>
2025-11-27 18:12:45 +0000

    refactor: streamline Prisma client initialization by removing redundant adapter variable

--- dal/prisma.ts
+++ dal/prisma.ts
@@ -2,16 +2,12 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/lib/generated/prisma/client";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
 const globalForPrisma = global as unknown as { prismaAdapter: PrismaClient };
 
 const prisma =
   globalForPrisma.prismaAdapter ||
   new PrismaClient({
-    adapter,
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
   });
 
 if (process.env.NODE_ENV !== "production") {
